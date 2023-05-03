b15b09faccae512e8af64e8c5998e2c291b5d03b
Kim-hyun-hee <81128882+Kim-hyun-hee@users.noreply.github.com>
2023-05-03 18:00:23 +0900

    맵

--- Assets/3.Script/Map/TubeSpawner.cs
+++ Assets/3.Script/Map/TubeSpawner.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class TubeSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject tubes;
     private ObjectPool objectPool;
     private float randomNum;
-
-    public void SetRandomPosition()
-    {
-        randomNum = Random.Range(9f, 20f);
-        objectPool.GetObject().transform.position = new Vector3(transform.position.x, randomNum, transform.position.z);
-        //transform.position = new Vector3(transform.position.x, randomNum, transform.position.z);
-        //Instantiate(tubes, transform);
-    }
+    [SerializeField] private float spawnTime;
 
     private void Start()
     {
-        SetRandomPosition();
+        GameObject.FindGameObjectWithTag("TubePool").TryGetComponent(out objectPool);
+        StartCoroutine(SpawnTube_co());
+    }
+
+    private void SetRandomPosition()
+    {
+        randomNum = Random.Range(9f, 20f);
+    }
+
+    private IEnumerator SpawnTube_co()
+    {
+        WaitForSeconds wfs = new WaitForSeconds(spawnTime);
+        while(true)
+        {
+            SetRandomPosition();
+            GameObject obj = objectPool.GetObject();
+            obj.transform.position = new Vector3(transform.position.x, randomNum, transform.position.z);
+            yield return wfs;
+        }
+
     }
 }
